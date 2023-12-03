f424cda58f68a4209bef758dbf38058eccff6e25
akadirhali <kadirhali07@gmail.com>
2023-12-03 15:12:35 +0300

    -

---
+++ DortIslem/Islemler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DortIslem
+{
+    
+    public class Islemler
+    {
+        public int topla(int sayi1,int sayi2)
+        { 
+            int sayi3;
+            sayi3 = sayi1 + sayi2;
+            Console.WriteLine("Toplam:" +sayi3);
+            return sayi3;
+        }
+        public int cikar(int sayi1, int sayi2)
+        {
+            int sayi3;
+            sayi3 = sayi1 - sayi2;
+            Console.WriteLine("Fark:" + sayi3);
+            return sayi3;
+        }
+        public int carp(int sayi1, int sayi2)
+        {
+            int sayi3;
+            sayi3 = sayi1 * sayi2;
+            Console.WriteLine("Çarpım:" + sayi3);
+            return sayi3;
+        }
+        public int böl(int sayi1, int sayi2)
+        {
+            int sayi3;
+            sayi3 = sayi1 / sayi2;
+            Console.WriteLine("Bölüm:" + sayi3);
+            return sayi3;
+        }
+    }
+}
---
+++ DortIslem/Program.cs
@@ -0,0 +1,7 @@
+﻿using DortIslem;
+
+Islemler islem = new Islemler();
+islem.topla(12,78);
+islem.cikar(78,4);
+islem.carp(14,4);
+islem.böl(12,3);
