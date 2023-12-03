c7eedf69ef35fbf0f95af7708131c13f7cfe8e90
akadirhali <kadirhali07@gmail.com>
2023-12-03 14:47:10 +0300

    Add project files.

---
+++ Void/Kisiler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void
+{
+    public class Kisiler
+    {
+        public void kisilerListesi(string adsoyad,int yas,string meslek)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{adsoyad} {yas} {meslek}");
+            }
+        }
+    }
+}
---
+++ Void/Program.cs
@@ -0,0 +1,13 @@
+﻿using Void;
+
+Kisiler ks = new Kisiler();
+string adsoyad,meslek;
+int yas;
+Console.WriteLine("Ad-Soyad Girin:");
+adsoyad=Console.ReadLine();
+Console.WriteLine("Yaş Girin:");
+yas = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Meslek Girin:");
+meslek = Console.ReadLine();
+ks.kisilerListesi(adsoyad,yas,meslek);
+Console.Read();
