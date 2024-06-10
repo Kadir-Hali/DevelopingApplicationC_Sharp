b65468a706d4ed0f4b9cd4160f5183925bbbefe2
Kadir Halı <kadirhali07@gmail.com>
2024-06-10 09:29:28 +0300

    Development Guide

---
+++ ConstructorMethod/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConstructorMethod;
+
+Kimlik kml = new Kimlik();
+Console.WriteLine(kml.AD);
+Console.WriteLine(kml.SOYAD);
+Console.WriteLine(kml.YAS);
+Console.WriteLine(kml.CINSIYET);
+Console.WriteLine(kml.MEMLEKET);
+
+Console.Read();
